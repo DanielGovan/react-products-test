21d2c18c7b72f5376af536bc23fd4432b8fe2245
Dan Govan <daniel.govan@gmail.com>
2020-08-12 17:04:52 +0100

    Products in page

--- src/App.js
+++ src/App.js
@@ -2,12 +2,34 @@ import React, { Component } from "react";
 
 import "./App.css";
 
+const branchPath = "http://localhost:3000/api/branch2.json";
+
 const formatNumber = (number) =>
   new Intl.NumberFormat("en", { minimumFractionDigits: 2 }).format(number);
+
 class App extends Component {
+  state = {
+    isFetching: true,
+    products: [],
+    searchKey: "",
+  };
+
+  componentDidMount() {
+    fetch(branchPath)
+      .then((res) => res.json())
+      .then((result) => {
+        this.setState({
+          isFetching: false,
+          products: result.products,
+        });
+      });
+  }
+
   render() {
+    const { products } = this.state;
+    console.log(products);
     return (
-      <div class="product-list">
+      <div className="product-list">
         <label>Search Products</label>
         <input type="text" />
 
@@ -18,7 +40,21 @@ class App extends Component {
               <th>Revenue</th>
             </tr>
           </thead>
-          <tbody></tbody>
+          <tbody>
+            {this.state.isFetching && (
+              <tr>
+                <td colSpan="2" style={{ textAlign: "center" }}>
+                  Fetching users...
+                </td>
+              </tr>
+            )}
+            {products.map((product) => (
+              <tr key={product.id}>
+                <td>{product.name}</td>{" "}
+                <td>{formatNumber(product.unitPrice * product.sold)}</td>
+              </tr>
+            ))}
+          </tbody>
           <tfoot>
             <tr>
               <td>Total</td>
